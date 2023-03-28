d7dd8e307936e3d437b6c8d2d501ea9ae58bbbf4
Heisenberg1007 <116086990+Heisenberg1007@users.noreply.github.com>
2023-03-28 13:50:13 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+    background("black")
+    
+    fill("#008000")
+    rect(0,0,width,90)
+
+    textAlign(CENTER)
+    fill("black")
+    //fontWeight="bold"
+    stroke("black")
+    strokeWeight(2)
+    textSize(50)
+    text("Tic Tac Toe",width/2,60)
+
+
+}
+
+
+
+
+function draw(){
+    
+}
+
